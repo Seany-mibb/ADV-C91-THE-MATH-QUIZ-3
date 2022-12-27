7f703e6ac5af637dc73d723faa647d67a47cda60
Baevver <86206802+Seany-mibb@users.noreply.github.com>
2022-12-26 19:53:39 -0800

    Add files via upload

--- quiz_page.js
+++ quiz_page.js
@@ -1,6 +1,9 @@
 player1_name = localStorage.getItem("player1_name");
 player2_name = localStorage.getItem("player2_name");
 
+var respond = 0;
+
+var actual_answer = 0;
 player1_score = 0;
 player2_score = 0;
 document.getElementById("player1_name").innerHTML = player1_name + ": ";
@@ -24,4 +27,44 @@ function send()
     document.getElementById("output").innerHTML = row;
     document.getElementById("number1").value = "";
     document.getElementById("number2").value = "";
+}
+
+var question_turn = 'player1';
+var answer_turn = 'player2';
+
+function check()
+{
+    respond = document.getElementById("input_check_box").value;
+    respond = parseInt(respond)
+    actual_answer = parseInt(number1) * parseInt(number2);
+    if(respond === actual_answer)
+    {
+        if(answer_turn == 'player1')
+        {
+            player1_score+=1;
+            question_turn = 'player1';
+            answer_turn = 'player2';
+            document.getElementById("player_question").innerHTML = "Question Turn - " + player1_name;
+            document.getElementById("player_answer").innerHTML = "Answer Turn - " + player2_name;
+            document.getElementById("player1_name").innerHTML = player1_name + ": ";
+            document.getElementById("player2_name").innerHTML = player2_name + ": ";
+            document.getElementById("player1_score").innerHTML = player1_score;
+            document.getElementById("player2_score").innerHTML = player2_score;
+            return answer_turn;//This return function is SUPER important because if you don't return it the score for the players with both add one because you didn't return it as it will always be the original and then add, then switch and add.
+        }
+
+        if(answer_turn == 'player2')
+        {
+            player2_score+=1;
+            question_turn = 'player2';
+            answer_turn = 'player1';
+            document.getElementById("player_question").innerHTML = "Question Turn - " + player2_name;
+            document.getElementById("player_answer").innerHTML = "Answer Turn - " + player1_name;
+            document.getElementById("player1_name").innerHTML = player1_name + ": ";
+            document.getElementById("player2_name").innerHTML = player2_name + ": ";
+            document.getElementById("player1_score").innerHTML = player1_score;
+            document.getElementById("player2_score").innerHTML = player2_score;
+            return answer_turn;//This return function is SUPER important because if you don't return it the score for the players with both add one because you didn't return it as it will always be the original and then add, then switch and add.
+        }
+    }
 }
